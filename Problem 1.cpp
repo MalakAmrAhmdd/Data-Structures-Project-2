d5e309faa4e842ea7a00d27552b084fe96f265c1
Ali Mohamed <162573315+AliMohamed805@users.noreply.github.com>
2025-05-08 00:55:19 +0300

    Update Problem 1.cpp

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -1,9 +1,123 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+struct Node {
+    string url;
+    Node* next;
+    Node(string u) : url(u), next(nullptr) {}
+};
 
-    cout << "Problem 1 executable" << endl;
-    cout <<"Yassin Elfekhem bimsi 3leko" << endl;
+class Stack {
+private:
+    Node* top;
 
+public:
+    Stack() : top(nullptr) {}
+
+    ~Stack() {
+        while (top != nullptr) {
+            Node* temp = top;
+            top = top->next;
+            delete temp;
+        }
+    }
+
+    void push(const string& url) {
+        Node* newNode = new Node(url);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    string pop() {
+        if (isEmpty()) return "";
+        Node* temp = top;
+        string url = top->url;
+        top = top->next;
+        delete temp;
+        return url;
+    }
+
+    string peek() const {
+        return isEmpty() ? "" : top->url;
+    }
+
+    bool isEmpty() const {
+        return top == nullptr;
+    }
+
+    void clear() {
+        while (!isEmpty()) {
+            pop();
+        }
+    }
+};
+
+class BrowserHistory {
+private:
+    Stack backStack;
+    Stack forwardStack;
+    string currentUrl;
+
+public:
+    BrowserHistory() : currentUrl("") {}
+
+    void visit(const string& url) {
+        if (!currentUrl.empty()) {
+            backStack.push(currentUrl);
+        }
+        currentUrl = url;
+        forwardStack.clear();
+    }
+
+    string goBack() {
+        if (backStack.isEmpty()) {
+            return "Cannot go back";
+        }
+        forwardStack.push(currentUrl);
+        currentUrl = backStack.pop();
+        return currentUrl;
+    }
+
+    string goForward() {
+        if (forwardStack.isEmpty()) {
+            return "Cannot go forward";
+        }
+        backStack.push(currentUrl);
+        currentUrl = forwardStack.pop();
+        return currentUrl;
+    }
+
+    string getCurrentUrl() const {
+        return currentUrl;
+    }
+};
+
+int main() {
+    BrowserHistory browser;
+    string inputFileName = "Problem_1.txt"; 
+
+    ifstream inputFile(inputFileName);
+
+    if (!inputFile.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return 1;
+    }
+
+    string command, url;
+    while (inputFile >> command) {
+        if (command == "visit") {
+            inputFile >> url;
+            browser.visit(url);
+            cout << "Visited: " << browser.getCurrentUrl() << endl;
+        } else if (command == "back") {
+            cout << "Back: " << browser.goBack() << endl;
+        } else if (command == "forward") {
+            cout << "Forward: " << browser.goForward() << endl;
+        } else {
+            cout << "Invalid command" << endl;
+        }
+    }
+
+    inputFile.close();
+    return 0;
 }
